11f2340c520d24546030229e665bfa0a62af48d6
Zhae Sei <33882778+ZhaeSei@users.noreply.github.com>
2021-10-24 13:09:29 -0600

    Hopefully merge isn't too bad

---
+++ wpfApp1/wpfApp1/Itinerary_Calendar.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace trvlApp
+{
+    /// <summary>
+    /// Interaction logic for Itinerary_Calendar.xaml
+    /// </summary>
+    public partial class Itinerary_Calendar : Window
+    {
+        public Itinerary_Calendar()
+        {
+            InitializeComponent();
+        }
+
+        private void Calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+           
+        }
+    }
+}
